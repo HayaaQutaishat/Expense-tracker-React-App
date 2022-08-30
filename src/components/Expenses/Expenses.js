28bd7b30b9f66707487e65c8757cea07fa99e536
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-08-30 13:43:02 +0200

    Arrow functions method

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,31 @@
-import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import "./Expenses.css";
+import Card from "../UI/Card";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
         title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
         date={props.expenses[0].date}
+        amount={props.expenses[0].amount}
       />
       <ExpenseItem
         title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
         date={props.expenses[1].date}
+        amount={props.expenses[1].amount}
       />
       <ExpenseItem
         title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
         date={props.expenses[2].date}
+        amount={props.expenses[2].amount}
       />
       <ExpenseItem
         title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
         date={props.expenses[3].date}
+        amount={props.expenses[3].amount}
       />
     </Card>
   );
-}
-
+};
 export default Expenses;
